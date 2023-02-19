7ef52a92ac66b193a40134e8dbaffb8c1d9e982d
abeznik <abeznik@student.codam.nl>
2023-02-19 13:32:54 +0100

    Finished id check + moved scenes and added new scenes + added textures

--- srcs/parser/check_map.c
+++ srcs/parser/check_map.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   check_map.c                                        :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: edawood <edawood@student.42.fr>            +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/02/19 10:54:21 by edawood           #+#    #+#             */
-/*   Updated: 2023/02/19 11:12:58 by edawood          ###   ########.fr       */
+/*                                                        ::::::::            */
+/*   check_map.c                                        :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: edawood <edawood@student.42.fr>              +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2023/02/19 10:54:21 by edawood       #+#    #+#                 */
+/*   Updated: 2023/02/19 13:23:26 by abeznik       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,13 @@
 
 bool	check_map(char *file_name, t_file_data *file_data)
 {
-    file_data->fd = open(file_name, O_RDONLY);
-    if (file_data->fd == ERROR)
-        return (logger(EXIT_FAILURE, "check_map", "Error opening file"), false);
-    file_data->line = read_file(file_data);
-    if (!file_data->line)
-        return (logger(EXIT_FAILURE, "check_map", "Error reading scene file"), false);
-    return (true);
+	file_data->fd = open(file_name, O_RDONLY);
+	if (file_data->fd == ERROR)
+		return (!error_msg("Opening scene file"));
+	file_data->line = read_file(file_data);
+	if (!file_data->line)
+		return (!error_msg("Reading scene file"));
+	if (!check_identifiers(file_data->identifiers, ".png"))
+		return (!error_msg("Invalid identifiers"));
+	return (true);
 }
--- srcs/parser/parser.c
+++ srcs/parser/parser.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   parser.c                                           :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: edawood <edawood@student.42.fr>            +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/02/12 16:56:05 by abeznik           #+#    #+#             */
-/*   Updated: 2023/02/19 11:28:14 by edawood          ###   ########.fr       */
+/*                                                        ::::::::            */
+/*   parser.c                                           :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: edawood <edawood@student.42.fr>              +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2023/02/12 16:56:05 by abeznik       #+#    #+#                 */
+/*   Updated: 2023/02/19 13:30:54 by abeznik       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,40 @@
 
 void	init_map_file_data(t_file_data *file_data)
 {
-	file_data->map_extention = ".cub";
+	t_identifiers_data	*id_data = (t_identifiers_data *)malloc(sizeof(t_identifiers_data));
+	t_rgb				*rgb_f = (t_rgb *)malloc(sizeof(t_rgb));
+	t_rgb				*rgb_c = (t_rgb *)malloc(sizeof(t_rgb));
+
+	file_data->map_extension = ".cub";
 	file_data->buflen = 1;
 	file_data->fd = 0;
+
+	id_data->north_texture = "./textures/empty.png";
+	id_data->south_texture = "./textures/empty.png";
+	id_data->west_texture = "./textures/empty.png";
+	id_data->east_texture = "./textures/empty.png";
+	
+	rgb_f->red = 220;
+	rgb_f->green = 100;
+	rgb_f->blue = 0;
+
+	rgb_c->red = 225;
+	rgb_c->green = 30;
+	rgb_c->blue = 0;
+
+	id_data->floor = rgb_f;
+	id_data->ceiling = rgb_c;
+
+	file_data->identifiers = id_data;
 }
 
 bool	parser(int argc, char *argv[], t_file_data *file_data)
 {
 	init_map_file_data(file_data);
 	if (argc != 2)
-		return (false);
-	if (!check_ext(argv[1], file_data))
+		// return (false);
+		return (!error_msg("Usage: ./cub3D <file_name>.cub"));
+	if (!check_ext(argv[1], file_data->map_extension))
 		return (false);
 	if (!check_map(argv[1], file_data))
 		return (false);
