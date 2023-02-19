e9acc57e29ac4d830084a70240b1b65987a46ac5
abeznik <abeznik@student.codam.nl>
2023-02-19 17:01:43 +0100

    Removed ! from return error_msg as it now returns bool + removed useless comment

--- srcs/parser/check_file_extension.c
+++ srcs/parser/check_file_extension.c
@@ -6,18 +6,12 @@
 /*   By: edawood <edawood@student.42.fr>              +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/02/15 12:33:38 by edawood       #+#    #+#                 */
-/*   Updated: 2023/02/19 14:43:11 by abeznik       ########   odam.nl         */
+/*   Updated: 2023/02/19 17:00:55 by abeznik       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <parser.h>
 
-/**
- * maybe we can pass the map extension directly instead of the whole struct
- * this way I can reuse this to check if the identifiers have png
- * 	OR
- * can we just verfiy the filename directly against ".cub" as its always going to be that
-*/
 bool	check_ext(char *file_name, char	*extension)
 {
 	size_t	ext_len;
