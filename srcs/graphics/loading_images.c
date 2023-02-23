18688cdc2d560c0db19da67de3856ce43a0f4f95
Essam Dawood <edawood@f1r1s11.codam.nl>
2023-02-23 14:49:08 +0100

    printed error msg when returning

--- srcs/graphics/loading_images.c
+++ srcs/graphics/loading_images.c
@@ -6,7 +6,7 @@
 /*   By: edawood <edawood@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/23 14:29:22 by edawood           #+#    #+#             */
-/*   Updated: 2023/02/23 14:36:30 by edawood          ###   ########.fr       */
+/*   Updated: 2023/02/23 14:48:46 by edawood          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,9 @@ bool    loading_images(mlx_texture_t **textures)
 {
     textures[WALL] = mlx_load_png("path/to/wall.png");
     if (!textures[WALL])
-        return (false);
+        return (error_msg("wall texture was not found"), false);
     textures[PLAYER] = mlx_load_png("path/to/player.png");
     if (!textures[PLAYER])
-        return (false);
+        return (error_msg("player texture was not found"), false);
     return (true);
 }
