c56affe87d915f929a82351236ca7db2c3bef198
Arthur Beznik <arthurbeznik@gmail.com>
2023-03-27 18:46:04 +0200

    Hitting horizontal lines woop woop

--- srcs/graphics/ray_caster.c
+++ srcs/graphics/ray_caster.c
@@ -8,25 +8,25 @@ bool draw_ray(t_general_data *data, double angle, int ray_x, int ray_y, int off_
 	double y1;
 
 	// fprintf(stderr, "rx | ry : %d | %d\n", ray_x, ray_y);
-	// mlx_put_pixel(data->graphics->img, ray_x, ray_y, 0x00FF0090);
-	d_size = 64;
 	i = 0;
 	x1 = 0;
+	y1 = 0;
 	while (i < 8 * PIXELS)
 	// while (x1 != ray_x)
 	{
 		// log_positions(data, "draw_dir", 'A');
-		x1 = (i * cos((angle))) + (data->file_data->player->x);
-		y1 = (i * sin((angle))) + (data->file_data->player->y);
+		x1 = round(i * cos((angle))) + (data->file_data->player->x); // ? this need to be rounded, like ray_x and _y
+		y1 = round(i * sin((angle))) + (data->file_data->player->y);
 		// log_positions(data, "draw_dir", 'D'); // ? testing
 		// log_positions(data, "draw_dir", 'P'); // ? testing
+		// fprintf(stderr, "x1 | y1: %f | %f\n", x1, y1); // ? testing
 		if (!check_put_pixel(data, x1, y1))
 			return (false);
-		// mlx_put_pixel(data->graphics->img, x1, y1, 0xFF0000FF);
-		mlx_put_pixel(data->graphics->img, ray_x, ray_y, 0xFF0000FF);
+		mlx_put_pixel(data->graphics->img, x1, y1, 0xFF0000FF);
+		if (y1 == ray_y) // ? only for horizontal lines
+			return (true);
 		i++;
 	}
-	// fprintf(stderr, "x1 | y1: %f | %f\n", x1, y1); // ? testing
 	return (true);
 }
 
